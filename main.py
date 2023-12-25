de716452d44c49b2ad54d4e5f42286a91653641b
Jiratta Areekhan <6610110046@email.psu.ac.th>
2023-12-25 22:32:12 +0700

    copy class char from original game

--- main.py
+++ main.py
@@ -6,6 +6,60 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 
+class Char:
+    def __init__(self, name, char_type, mana, hp):
+        self.name = name
+        self.char_type = char_type
+        self.mana = mana
+        self.hp = hp
+
+    def get_mana(self):
+        return self.mana
+
+    def get_hp(self):
+        return self.hp
+
+    def print_info(self):
+        print("Name:", self.name)
+        print('Type:', self.char_type)
+        print('Mana:', self.mana)
+        print('HP:', self.hp)
+
+    def skill_with_damage_and_mana(self):
+        characters = {
+            'Karina': [('Skill 1', 30, 20), ('Skill 2', 40, 30), ('Skill 3', 50, 40)],
+            'Winter': [('Skill 1', 25, 15), ('Skill 2', 35, 25), ('Skill 3', 45, 35)],
+            'Giselle': [('Skill 1', 35, 25), ('Skill 2', 45, 35), ('Skill 3', 55, 45)],
+            'Ningning': [('Skill 1', 20, 10), ('Skill 2', 30, 20), ('Skill 3', 40, 30)]
+        }
+
+        for skill, damage, mana in characters[self.name]:
+            print(f"{skill} damage {damage} mana {mana}")
+
+    def attack(self, target, skill_number):
+        skills = {
+            'Karina': [30, 40, 50],
+            'Winter': [25, 35, 45],
+            'Giselle': [35, 45, 55],
+            'Ningning': [20, 30, 40]
+        }
+
+        if 1 <= skill_number <= 3:
+            damage = skills[self.name][skill_number - 1]
+            mana_cost = Char[self.name][skill_number - 1][2]
+
+            if self.mana >= mana_cost:
+                target.hp -= damage
+                self.mana -= mana_cost
+
+        self.hp = max(self.hp, 0)
+        self.mana = max(self.mana, 0)
+
+# Instances of characters
+Karina = Char('Karina', 'Rocket puncher', 100, 100)
+Winter = Char('Winter', 'armamenter', 100, 100)
+Giselle = Char('Giselle', 'xenoglossy', 100, 100)
+Ningning = Char('Ningning', 'xenoglossy', 100, 100)
 
 
 class BattleApp(App):
