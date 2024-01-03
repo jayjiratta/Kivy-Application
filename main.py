3da263eda4e1af1a7475fa2fb4a87b1c9e46703a
Jiratta Areekhan <6610110046@email.psu.ac.th>
2024-01-03 20:46:07 +0700

    first page 80% wait for skill

--- main.py
+++ main.py
@@ -65,7 +65,6 @@ Ningning = Char('Ningning', 'xenoglossy', 100, 100)
 
 class MainApp(App):
     def build(self):
-
         # Create screens
         screen_manager = ScreenManager()
 
@@ -73,29 +72,50 @@ class MainApp(App):
         welcome_screen = Screen(name="welcome")
         welcome_layout = BoxLayout(orientation='vertical')
         welcome_label = Label(text="Welcome to MYs world!!!")
-        start_button = Button(text="Start Game")
-        welcome_layout.add_widget(welcome_label)
-        welcome_layout.add_widget(start_button)
-        welcome_screen.add_widget(welcome_layout)
-        screen_manager.add_widget(welcome_screen)
-
+        
+        # Char details layout
+        char_detail_layout = BoxLayout(orientation='horizontal')
+        
+        # Player 1
         player1_label = Label(text="Karina")
-        player1_image = Image(source="./image/Karina.jpg") 
+        player1_image = Image(source="./image/Karina.jpg")
+        player1_layout = BoxLayout(orientation='vertical')
+        player1_layout.add_widget(player1_image)
+        player1_layout.add_widget(player1_label)
+        char_detail_layout.add_widget(player1_layout)
+        
+        # Player 2
         player2_label = Label(text="Winter")
         player2_image = Image(source="./image/Winter.jpg")
-        player3_label = Label(text="Giselle")
-        player3_image = Image(source="./image/Giselle.jpg") 
-        player4_label = Label(text="Ningning")
-        player4_image = Image(source="./image/Ningning.jpg") 
+        player2_layout = BoxLayout(orientation='vertical')
+        player2_layout.add_widget(player2_image)
+        player2_layout.add_widget(player2_label)
+        char_detail_layout.add_widget(player2_layout)
         
-        welcome_layout.add_widget(player1_label)
-        welcome_layout.add_widget(player1_image)
-        welcome_layout.add_widget(player2_label)
-        welcome_layout.add_widget(player2_image)
-        welcome_layout.add_widget(player3_label)
-        welcome_layout.add_widget(player3_image)
-        welcome_layout.add_widget(player4_label)
-        welcome_layout.add_widget(player4_image)
+        # Player 3
+        player3_label = Label(text="Giselle")
+        player3_image = Image(source="./image/Giselle.jpg")
+        player3_layout = BoxLayout(orientation='vertical')
+        player3_layout.add_widget(player3_image)
+        player3_layout.add_widget(player3_label)
+        char_detail_layout.add_widget(player3_layout)
+        
+        # Player 4
+        player4_label = Label(text="Ningning")
+        player4_image = Image(source="./image/Ningning.jpg")
+        player4_layout = BoxLayout(orientation='vertical')
+        player4_layout.add_widget(player4_image)
+        player4_layout.add_widget(player4_label)
+        char_detail_layout.add_widget(player4_layout)
+        
+        welcome_layout.add_widget(welcome_label)
+        welcome_layout.add_widget(char_detail_layout)
+        
+        start_button = Button(text="Start Game")
+        welcome_layout.add_widget(start_button)
+
+        welcome_screen.add_widget(welcome_layout)
+        screen_manager.add_widget(welcome_screen)
 
         return screen_manager
 
